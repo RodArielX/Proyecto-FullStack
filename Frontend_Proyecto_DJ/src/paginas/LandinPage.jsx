fa67b9f088036a5a5995b6a05885db6b546eeabd
RodArielX <ariel.ashqui@epn.edu.ec>
2025-02-10 06:28:05 -0500

    modificacion-ladinpage

--- Frontend_Proyecto_DJ/src/paginas/LandinPage.jsx
+++ Frontend_Proyecto_DJ/src/paginas/LandinPage.jsx
@@ -12,6 +12,15 @@ import logoWeb3 from '../assets/web3.png'
 import logoWeb4 from '../assets/web4.png'
 import logoWeb5 from '../assets/web5.png'
 import logoWeb6 from '../assets/web6.png'
+
+import proyecto1 from '../assets/proyecto1.jpg'
+import proyecto2 from '../assets/proyecto2.jpg'
+import proyecto3 from '../assets/proyecto3.jpg'
+import proyecto4 from '../assets/proyecto4.jpg'
+import proyecto5 from '../assets/proyecto5.jpg'
+import proyecto6 from '../assets/proyecto6.jpg'
+
+
 import { useState } from 'react'
 import {Link} from 'react-router-dom'
 
@@ -24,7 +33,7 @@ export const LandinPage = () => {
             <main className='bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-800'>
                 <section>
                     <nav className='p-10 mb-12 flex justify-between'>
-                        <h1 className='text-2xl font-bold dark:text-white'>App-Demo</h1>
+                        <h1 className='text-2xl font-bold dark:text-white'>EDWIN ASQUI DJ</h1>
                         <ul className='flex items-center'>
                             <li><img onClick={()=>setdarkMode(!darkMode)} className='cursor-pointer' src={logoDarkMode} alt="logo" width={40} height={40}/></li>
                             <li><Link to="/login" className='bg-gray-600 text-slate-400 px-6 py-2 rounded-full ml-8 hover:bg-gray-900 hover:text-white' href="#">Login</Link></li>
@@ -53,13 +62,12 @@ export const LandinPage = () => {
 
                 <section>
                     <div>
-                        <h3 className='text-3xl py-1 dark:text-white'>Servicios Ofrecidos</h3>
+                        <h3 className='text-3xl py-1 dark:text-white'>Proyectos</h3>
                         <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>Ha trabajado en varios eventos como <span className='text-teal-500'>cumpleaños, bautizos, graduaciones</span>, etc. En otras palabras, tiene la experiencia de trabajar en todo tipo de evento social.</p>
                         <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>EDWIN ASQUI DJ es especializado en <span className='text-teal-500'>todos los géneros musicales.</span>Por lo tanto, si el cliente solicita algún género en especial no dude que <span className='text-teal-500'>Edwin</span> lo deleitará con lo requerido.</p>
-                        Por lo tanto, si el cliente solicita algún género en especial no dude que Edwin lo deleitará con lo requerido.
                     </div>
 
-                    <div className='md:flex md:flex-wrap lg:flex lg:justify-center gap-10'>
+                    {/*<div className='md:flex md:flex-wrap lg:flex lg:justify-center gap-10'>
                         <div className='text-center shadow-2xl p-10 rounded-xl my-10 md:w-72 lg:w-96 dark:bg-slate-100'>
                             <img className='mx-auto' src={logoCode} alt="" />
                             <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
@@ -87,52 +95,52 @@ export const LandinPage = () => {
                             <p className='text-gray-800 py-1'>Illustrator</p>
                             <p className='text-gray-800 py-1'>Figma</p>
                         </div>
-                    </div>
+                    </div>*/}
                 </section>
 
                 <section>
-                    <div>
+                    {/*<div>
                         <h3 className='text-3xl py-1 dark:text-white'>Proyects</h3>
                         <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae, delectus iure <span className='text-teal-500'>quibusdam</span> quas quaerat itaque, est minima ducimus recusandae illo ipsam numquam nam earum libero <span className='text-teal-500'>deleniti</span> voluptatem! Autem, veniam ut.</p>
                         <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae, delectus iure <span className='text-teal-500'>quibusdam</span> quas quaerat itaque, est minima ducimus recusandae illo ipsam numquam nam earum libero <span className='text-teal-500'>deleniti</span> voluptatem! Autem, veniam ut.</p>
-                    </div>
+                    </div>*/}
 
 
                     <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
                         <div className="basis-1/3 flex-1 ">
                             <img
                                 className="rounded-lg object-cover"
-                                src={logoWeb1}
+                                src={proyecto1}
                             />
                         </div>
                         <div className="basis-1/3 flex-1">
                             <img
                                 className="rounded-lg object-cover"
-                                src={logoWeb2}
+                                src={proyecto2}
                             />
                         </div>
                         <div className="basis-1/3 flex-1">
                             <img
                                 className="rounded-lg object-fill"
-                                src={logoWeb4}
+                                src={proyecto3}
                             />
                         </div>
                         <div className="basis-1/3 flex-1">
                             <img
                                 className="rounded-lg object-cover"
-                                src={logoWeb6}
+                                src={proyecto4}
                             />
                         </div>
                         <div className="basis-1/3 flex-1">
                             <img
                                 className="rounded-lg object-cover"
-                                src={logoWeb5}
+                                src={proyecto5}
                             />
                         </div>
                         <div className="basis-1/3 flex-1 lg:bg-orange-100">
                             <img
                                 className="rounded-lg object-cover "
-                                src={logoWeb3}
+                                src={proyecto6}
                             />
                         </div>
                     </div>
